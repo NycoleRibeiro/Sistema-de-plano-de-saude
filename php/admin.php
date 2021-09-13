152043015c2dfd20a7d1d52557179d5cf1bbb93a
Nycole Ribeiro <rpnyck@gmail.com>
2021-09-13 13:44:24 -0300

    Create php inicial

--- php/admin.php
+++ php/admin.php
@@ -11,3 +11,16 @@ function cadastrar_medico($nome, $crm, $especialidade, $telefone, $email, $ender
 function cadastrar_laboratório($nome, $cnpj, $tipo_exame, $telefone, $email, $endereco, $senha) {
 
 }
+
+
+if(isset($_POST['cadastro_lab'])) {
+    echo "Cadastro de laboratório";
+    $nome = $_POST['nome'];
+    $cnpj = $_POST['cnpj'];
+    $tipo_exame = $_POST['tipo_exame'];
+    $telefone = $_POST['telefone'];
+    $email = $_POST['email'];
+    $endereco = $_POST['endereco'];
+    $senha = $_POST['senha'];
+    cadastrar_laboratório($nome, $cnpj, $tipo_exame, $telefone, $email, $endereco, $senha);
+}
