6b508d172ae14e0cf5487b2279489d1412097142
Nycole Ribeiro <rpnyck@gmail.com>
2021-09-26 21:26:45 -0300

    Update laboratorio.php

--- php/laboratorio.php
+++ php/laboratorio.php
@@ -10,10 +10,8 @@
 </head>
 <body>
     <?php
-        function editarDados($nome, $email, $senha, $telefone, $endereco, $tipo_exame) {
-            $LoginAtual = fopen("../banco_de_dados/login.txt", "r");
-            $cnpj = fgets($LoginAtual);
-            fclose($LoginAtual);
+        function editarDados($nome, $email, $senha, $telefone, $endereco, $tipo_exame, $cnpj) {
+            
             $caminho_completo = "../banco_de_dados/laboratorios/" . $cnpj . "/dados.xml";
             $xml = simplexml_load_file($caminho_completo);
             if ($xml === false) {
@@ -34,10 +32,50 @@
             }
         }
 
-        function cadastrarExame() {
+        function cadastrarExame($data, $cpf, $tipo_exame, $resultado, $cnpj) {
+            $caminho = "../banco_de_dados/pacientes/" . $cpf;
+            if (!is_dir($caminho)) {
+                echo '<div class="error"><a href="../laboratorio_exames.html"><i class="fas fa-arrow-circle-left"></i></a> <h1>Não existe um paciente com esse CPF</h1></div>';
+            } else {
+                $exame = 
+                "<?xml version='1.0' encoding='UTF-8'?>
+                <exame>
+                    <data></data>
+                    <laboratorio></laboratorio>
+                    <cpf></cpf>
+                    <tipo_exame></tipo_exame>
+                    <resultado></resultado>
+                </exame>";
+                $nome_arquivo = "/exame_de_" . $tipo_exame . "_" . $data . ".xml";
+                $arquivo = fopen($caminho . $nome_arquivo, "w");
+                fwrite($arquivo, $exame);
+                fclose($arquivo);
 
+                $xml = simplexml_load_file($caminho . $nome_arquivo);
+                if ($xml === false) {
+                    echo "Erro ao carregar XML";
+                    foreach(libxml_get_errors() as $error) {
+                        echo "<br>", $error->message;
+                    }
+                } else {
+                    $xml->data = $data;
+                    $xml->laboratorio = $cnpj;
+                    $xml->cpf = $cpf;
+                    $xml->tipo_exame = $tipo_exame;
+                    $xml->resultado = $resultado;
+                    $save = simplexml_import_dom($xml);
+                    $save->saveXML($caminho . $nome_arquivo);
+                    echo '<div class="ok"><a href="../laboratorio_exames.html"><i class="fas fa-arrow-circle-left"></i></a> <h1>Exame cadastrado com sucesso!</h1></div>';
+                }
+            }
         }
 
+
+        // Pegando o login atual
+        $LoginAtual = fopen("../banco_de_dados/login.txt", "r");
+        $cnpj = fgets($LoginAtual);
+        fclose($LoginAtual);
+        // Pegando os dados do laboratorio
         if (isset($_POST['saveDados'])) {
             $nome = $_POST['labName'];
             $email = $_POST['email'];
@@ -45,9 +83,13 @@
             $telefone = $_POST['phone'];
             $endereco = $_POST['address'];
             $tipo_exame = $_POST['examTypes'];
-            editarDados($nome, $email, $senha, $telefone, $endereco, $tipo_exame);
-        } else if (isset($_POST['cadastrarExam'])) {
-
+            editarDados($nome, $email, $senha, $telefone, $endereco, $tipo_exame, $cnpj);
+        } else if (isset($_POST['cadastrarExame'])) {
+            $data = $_POST['data'];
+            $cpf = $_POST['cpf'];
+            $tipo_exame = $_POST['exame'];
+            $resultado = $_POST['resultado'];
+            cadastrarExame($data, $cpf, $tipo_exame, $resultado, $cnpj);
         }
     ?>
 </body>
